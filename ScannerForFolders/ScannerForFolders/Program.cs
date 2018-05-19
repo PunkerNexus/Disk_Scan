e6da3b742dce0349afe096168fd6135ce4cc1995
Pasha Dedukh <castiel.galante@gmail.com>
2018-05-19 23:02:37 +0300

    with comments, wrong version

---
+++ ScannerForFolders/ScannerForFolders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerForFolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // один из вариантов
+            // попытка сделать с EnumerateFiles
+            // ищет до момента, пока не словит exception
+            // в класс Scanner добавлять нечего
+            Console.WriteLine("Enter the folder name(like 'c:\\', 'e:\\', 'd:\\' or another folder you have): ");
+            string directory = Console.ReadLine();
+
+            try
+            {
+                foreach (string file in Directory.EnumerateFiles(@directory, "*.*", SearchOption.AllDirectories))
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+            Console.ReadKey();
+        }
+    }
+}
